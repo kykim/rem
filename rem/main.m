d5ca1c77b4d93cff2f33727ec60a418af74a5d6b
Kevin Y. Kim <kykim@yahoo.com>
2012-10-10 17:04:10 -0400

    Initial Commit

---
+++ rem/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  rem
+//
+//  Created by Kevin Y. Kim on 10/10/12.
+//  Copyright (c) 2012 kykim, inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
